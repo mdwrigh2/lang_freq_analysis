47a5b7c2e9d639f5732cfa74d96ae485543db0b4
Michael Wright <mdwrigh2@ncsu.edu>
2010-03-12 00:22:13 -0500

    octopi still borked when pulling projects clone urls

--- freq_analysis.rb
+++ freq_analysis.rb
@@ -1,11 +1,17 @@
 require 'octopi'
 
 languages = %w{ Java Ruby Objective-C C Erlang JavaScript Perl Python Shell PHP C C++ }
-
+include Octopi
 def analyze language
   # Check and create appropriate directories (PROJ_ROOT/src/#{language}/, PROJ_ROOT/analysis/#{language}/, etc.
-  repos = Octopi::Repository.find_all "language:#{language}"
-  repos.each do |repo|
-    puts repo.name
+  authenticated do |g|
+    repos = Repository.find_all "language:#{language}"
+    repos.each do |repo|
+      r = g.repository(repo.name)
+      puts r.clone_url
+    end
   end
 end
+
+
+analyze languages[0]
